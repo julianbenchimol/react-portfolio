8e85867f35490452d62121421b1a06c3f1b44e57
JulianBenchimol <jbenchimol1999@gmail.com>
2023-03-04 10:16:59 -0500

    added projects page

---
+++ src/components/Projects.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Projects(){
+    return(
+        <div>
+            <h1 className="text-center">No Projects Added Yet!</h1>
+        </div>
+    )
+}
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,12 +1,12 @@
 import React from "react"
-import Project from '../Project'
+import Projects from '../Project'
 
 export default function Portfolio(props){
     return(
         <div>
             <h1>My Projects</h1>
 
-            <Project/>
+            <Projects/>
         </div>
     )
 }
