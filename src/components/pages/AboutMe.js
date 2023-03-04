2d05a1228cb5f032b0baf5b39a902fdceb78811e
JulianBenchimol <jbenchimol1999@gmail.com>
2023-03-04 09:51:25 -0500

    about me page created

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+    return(
+    <div>
+        <h1 className="text-center">About Me</h1>
+            <div className="col-6 ms-5 me-4">    
+                <p id="body">My name is Julian Benchimol and I am a Game Designer turned web developer!
+                I have been learning coding since 2017 and even before then learning simple C# scripting using
+                game development tools. Since graduation, I have been looking to branch out my abilities and 
+                web development seemeed to be the best course of action!
+                </p>
+            </div>
+    </div>
+    )
+}
