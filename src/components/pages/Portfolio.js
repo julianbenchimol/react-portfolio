b800e407a635ccf8c13140468b6aa2d77ee89da2
JulianBenchimol <jbenchimol1999@gmail.com>
2023-03-04 10:14:35 -0500

    Portfolio Page created

---
+++ src/components/pages/Portfolio.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Project from '../Project'
+
+export default function Portfolio(props){
+    return(
+        <div>
+            <h1>My Projects</h1>
+
+            <Project/>
+        </div>
+    )
+}
